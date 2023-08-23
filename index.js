27f3ee9080e72b16fa33ed6fbbe22c58b45b9016
maitreyyi <matreyisinha@gmail.com>
2023-08-22 18:13:32 -0700

    form submission works and list of activities show

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import express from "express";
 import ejs from "ejs";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var activities = {};
+
+const d = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+let day = days[d.getDay()];
+let date = d.getDate();
+let month = months[d.getMonth()];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
 });
 
 app.get('/', (req,res) => {
-    res.render('index.js');
+    res.render("index.ejs", {
+        day: day, 
+        date: date,
+        month: month,
+    });
 });
 
+app.post('/submit', (req,res) => {
+    var activity = req.body["activity"];
+    //console.log(activity);
+    
+    activities[activity] = true;
+
+    res.render("index.ejs", {
+        day: day, 
+        date: date,
+        month: month,
+        activities: activities,
+    });
+    
+});
